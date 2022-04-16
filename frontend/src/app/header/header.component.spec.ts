9cfbaf596932ef9ceb2bed0fadaf4ca94dafd6c8
Jackson Neves <jacolpn@outlook.com>
2022-04-15 23:58:24 -0300

    2-1º-step-cabeçalho-header - Create test unitary

--- frontend/src/app/header/header.component.spec.ts
+++ frontend/src/app/header/header.component.spec.ts
@@ -2,24 +2,51 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe(HeaderComponent.name, () => {
+    let component: HeaderComponent;
+    let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ HeaderComponent ]
+        })
+        .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(HeaderComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('Should create component', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it(`(D) Should display "userLogged" when bound to properties`, () => {
+        let user = 'Test Unitary';
+
+        component.user = true;
+        component.userName = user;
+        fixture.detectChanges();
+
+        const userLogged: HTMLImageElement = fixture.nativeElement.querySelector('#user-logged');
+        const login: HTMLImageElement = fixture.nativeElement.querySelector('#login');
+        const userName: HTMLImageElement = fixture.nativeElement.querySelector('span');
+
+        expect(userLogged).toBeTruthy();
+        expect(login).toBeNull();
+        expect(userName.innerText).toBe(user);
+    });
+
+    it(`(D) Should display "login" when bound to properties`, () => {
+        component.user = false;
+        fixture.detectChanges();
+
+        const userLogged: HTMLImageElement = fixture.nativeElement.querySelector('#user-logged');
+        const login: HTMLImageElement = fixture.nativeElement.querySelector('#login');
+
+        expect(login).toBeTruthy();
+        expect(userLogged).toBeNull();
+    });
 });
